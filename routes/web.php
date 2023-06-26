faeb3007c8339b40c5eef7f6f11091d5099105b1
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-26 16:03:01 +0200

    Url changed

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
+Route::get('/about-page', function () {
     return view('about');
 });
-Route::get('/shop', function () {
+Route::get('/shop-page', function () {
     return view('shop');
 });
-Route::get('/contact', function () {
+Route::get('/contact-page', function () {
     return view('contact');
 });
