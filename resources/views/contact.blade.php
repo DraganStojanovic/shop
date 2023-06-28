c40e28d51d7f5433f49280f026b65f022a2fbb7b
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-28 20:27:42 +0200

    Domaci zadatak

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,8 +3,47 @@
 
     @section( 'content' )
         <div class="container pt-3">
-            <div class="row">
+            <div class="row text-center">
                 <h1>Contact Page</h1>
             </div>
+                <!-- Wrapper container -->
+                <div class="container py-4">
+
+                    <!-- Bootstrap 5 starter form -->
+                    <form id="contactForm">
+
+                        <!-- Name input -->
+                        <div class="col-md-6 offset-md-3">
+                            <label class="form-label" for="your-subject">Your Subject</label>
+                            <input class="form-control" id="your-subject" name="subject" type="text" placeholder="Your Subject" />
+                        </div>
+
+                        <!-- Email address input -->
+                        <div class="col-md-6 offset-md-3">
+                            <label class="form-label" for="emailAddress">Email Address</label>
+                            <input class="form-control" id="emailAddress" name="email" placeholder="Email Address" />
+                        </div>
+
+                        <!-- Message input -->
+                        <div class="col-md-6 offset-md-3">
+                            <label class="form-label" for="message">Message</label>
+                            <textarea class="form-control" id="message" name="message" placeholder="Message" style="height: 10rem;"></textarea>
+                        </div>
+
+                        <!-- Form submit button -->
+                        <div class="col-md-6 offset-md-3 d-grid">
+                            <button class="btn btn-primary btn-lg" type="submit">Submit</button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+
+        <div class="container-fluid px-0">
+            <div class="row col-12 g-0">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2838.2456886591676!2d20.184314076019966!3d44.65333177107229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a1463f770ac03%3A0xeadc1683a5c2076e!2zVm9qdm9kZSBNacWhacSHYQ!5e0!3m2!1sen!2srs!4v1687976386392!5m2!1sen!2srs" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
         </div>
     @stop
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,24 +1,47 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
+
 @include("includes.navigation")
 
 @yield('content')
 
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 </body>
 @include('includes.footer')
 </html>
+{{--<!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <meta name="viewport"--}}
+{{--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
+{{--    <title>@yield('title')</title>--}}
+{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"--}}
+{{--          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--@include("includes.navigation")--}}
+
+{{--@yield('content')--}}
+
+
+{{--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>--}}
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"--}}
+{{--        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>--}}
+{{--</body>--}}
+{{--@include('includes.footer')--}}
+{{--</html>--}}
