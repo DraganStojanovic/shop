7cf1490ed48d4b2c0a2c5351679b535335109e6a
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-28 15:37:23 +0200

    Footer done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
                 <div class="row">
                     <h1>Home page</h1>
                 </div>
+                <div class="row">
+                    <div class="jumbotron">
+                        <h3>Trenutno vreme je:</h3>
+                        <h4>{{ date("h:i:s") }}</h4>
+                    </div>
+                </div>
             </div>
     @stop
 
