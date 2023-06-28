1a3bcc827816c9b6ce8947738dc9e2dd6bb40db6
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-28 15:27:31 +0200

    Footer done

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class="page-footer font-small bg-light fixed-bottom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2023 Copyright:
+        <a href="/"> itskola.net</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
