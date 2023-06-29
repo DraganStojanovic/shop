2f3fa792a0dfb63d47268879b83511bede0860c0
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-29 11:34:03 +0200

    Domaci zadatak uklanjanje jumbotrona u bootstrap 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,9 @@
                     <h1>Home page</h1>
                 </div>
                 <div class="row">
-                    <div class="jumbotron">
-                        <h3>Trenutno vreme je:</h3>
-                        <h5>{{ date("h:i:s") }}</h5>
+                    <div class="bg-light p-5 rounded-lg m-3">
+                        <h3 class="display-4">Trenutno vreme je:</h3>
+                        <h5 class="display-2">{{ date("h:i:s") }}</h5>
                     </div>
                 </div>
             </div>
