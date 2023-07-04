72db9a532a613a008f92db9f30bad3b7e9ebc35c
DraganStojanovic <draganstojanovic3@gmail.com>
2023-07-04 13:30:07 +0200

    Controller2

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,17 +2,25 @@
 @section('title', 'Shop')
 
     @section( 'content' )
-        <div class="container pt-3">
-            <div class="row">
+
+        <div class="card p-2" style="width: 28rem;">
+            <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+            <div class="card-body">
                 @foreach($products as $product)
 
-                    <h1>Product: {{ $product->name }}</h1><br>
-                    <h3>Model: {{ $product->description }}</h3><br>
-                    <h4>Amount: {{ $product->amount }}</h4><br>
-                    <h5>Price: {{ $product->price }}</h5>
-
-                @endforeach
+                    <h1 class="card-title">Product: {{ $product->name }}</h1><br>
+                    <h3 class="card-text">Model: {{ $product->description }}</h3><br>
 
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Amount: {{ $product->amount }}</li>
+                <li class="list-group-item">Price: {{ $product->price }}</li>
+
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">More</a>
+                <a href="#" class="card-link">Compare</a>
+            </div>
         </div>
+        @endforeach
     @stop
