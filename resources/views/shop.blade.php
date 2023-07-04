be61d8c832e434af844c77329a12e266d0d16ff8
DraganStojanovic <draganstojanovic3@gmail.com>
2023-07-04 13:08:25 +0200

    Controllers 1

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,7 +4,15 @@
     @section( 'content' )
         <div class="container pt-3">
             <div class="row">
-                <h1>Shop Page</h1>
+                @foreach($products as $product)
+
+                    <h1>Product: {{ $product->name }}</h1><br>
+                    <h3>Model: {{ $product->description }}</h3><br>
+                    <h4>Amount: {{ $product->amount }}</h4><br>
+                    <h5>Price: {{ $product->price }}</h5>
+
+                @endforeach
+
             </div>
         </div>
     @stop
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,13 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::view('/', "welcome");
-Route::view('/about', "about");
-Route::view('/shop', "shop");
-Route::view('/contact', "contact");
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+
+Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
+
+Route::get('/shop',[\App\Http\Controllers\ProductController::class, 'index']);
+
+
+
 
 
