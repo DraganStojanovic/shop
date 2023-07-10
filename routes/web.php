22db4f7ff95f4e3b6ab22443414821b7de2cc023
DraganStojanovic <draganstojanovic3@gmail.com>
2023-07-10 13:08:49 +0200

    Removed - comments from web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+
 Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
+
 Route::get('/shop',[\App\Http\Controllers\ProductController::class, 'index']);
+
 Route::get('/admin/all-contacts', [\App\Http\Controllers\ContactController::class, 'getAllContacts']);
 
 
