fc1377a7b3386e4c8364b35a2e03c4d357d9bf44
DraganStojanovic <draganstojanovic3@gmail.com>
2023-08-11 15:22:03 +0200

    Get back from history web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+
+Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
+
+Route::get('/shop', [\App\Http\Controllers\ProductController::class, 'index']);
+
+Route::get('/admin/all-contacts', [\App\Http\Controllers\ContactController::class, 'getAllContacts']);
+Route::post('/send-contact', [\App\Http\Controllers\ContactController::class, 'sendContact']);
+Route::get('/admin/delete-contact/{contact}', [\App\Http\Controllers\ContactController::class, 'delete'])->name('obrisiContact');
+Route::get('/admin/edit-contact/edit/{id}', [\App\Http\Controllers\ContactController::class, 'singleContact'])->name('contact.single');
+Route::post('/admin/edit-contact/save/{id}', [\App\Http\Controllers\ContactController::class, 'save'])->name('contact.save');
+
+
+Route::get('/admin/all-product', [\App\Http\Controllers\ProductController::class, 'getAllProducts']);
+Route::post('/send-product', [\App\Http\Controllers\ProductController::class, 'sendProduct']);
+Route::get('/admin/products', [\App\Http\Controllers\ProductController::class, 'sendAdminProducts']);
+
+Route::get('/admin/delete-product/{product}', [\App\Http\Controllers\ProductController::class, 'delete'])->name('obrisiProizvod');
+/**
+ * Editovanje ili izmena single product-a !
+ */
+Route::get('/admin/edit-product/edit/{id}', [\App\Http\Controllers\ProductController::class, 'singleProduct'])->name('product.single');
+/**
+ * Updagte izmene product-a u bazu !!!
+ */
+Route::post('/admin/edit-product/save/{id}', [\App\Http\Controllers\ProductController::class, 'save'])->name('product.save');
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
