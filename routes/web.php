688e84a1b8e0cfd2bef689888d1193058727d4e1
DraganStojanovic <draganstojanovic3@gmail.com>
2023-08-26 11:48:30 +0200

    Sredjivanje Middleware, kreiranje i refaktorisanje code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,35 +18,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
 
-Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/shop', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::get('/shop', [ProductController::class, 'index']);
 
-Route::get('/admin/all-contacts', [\App\Http\Controllers\ContactController::class, 'getAllContacts']);
-Route::post('/send-contact', [\App\Http\Controllers\ContactController::class, 'sendContact']);
-Route::get('/admin/delete-contact/{contact}', [\App\Http\Controllers\ContactController::class, 'delete'])->name('obrisiContact');
-Route::get('/admin/edit-contact/edit/{id}', [\App\Http\Controllers\ContactController::class, 'singleContact'])->name('contact.single');
-Route::post('/admin/edit-contact/save/{id}', [\App\Http\Controllers\ContactController::class, 'save'])->name('contact.save');
+Route::middleware('auth')->prefix('admin')->group(function(){
+    Route::get('/all-contacts', [ContactController::class, 'getAllContacts']);
+    Route::post('/send-contact', [ContactController::class, 'sendContact']);
+    Route::get('/delete-contact/{contact}', [ContactController::class, 'delete'])->name('obrisiContact');
+    Route::get('/edit-contact/edit/{id}', [ContactController::class, 'singleContact'])->name('contact.single');
+    Route::post('/edit-contact/save/{id}', [ContactController::class, 'save'])->name('contact.save');
 
 
-Route::get('/admin/all-product', [\App\Http\Controllers\ProductController::class, 'getAllProducts']);
-Route::post('/send-product', [\App\Http\Controllers\ProductController::class, 'sendProduct']);
-Route::get('/admin/products', [\App\Http\Controllers\ProductController::class, 'sendAdminProducts']);
+    Route::get('/all-product', [ProductController::class, 'getAllProducts']);
+    Route::post('/send-product', [ProductController::class, 'sendProduct']);
+    Route::get('/products', [ProductController::class, 'sendAdminProducts']);
 
-Route::get('/admin/delete-product/{product}', [\App\Http\Controllers\ProductController::class, 'delete'])->name('obrisiProizvod');
-/**
- * Editovanje ili izmena single product-a !
- */
-Route::get('/admin/edit-product/edit/{id}', [\App\Http\Controllers\ProductController::class, 'singleProduct'])->name('product.single');
-/**
- * Updagte izmene product-a u bazu !!!
- */
-Route::post('/admin/edit-product/save/{id}', [\App\Http\Controllers\ProductController::class, 'save'])->name('product.save');
-
+    Route::get('/delete-product/{product}', [ProductController::class, 'delete'])->name('obrisiProizvod');
+    /**
+     * Editovanje ili izmena single product-a !
+     */
+    Route::get('/edit-product/edit/{id}', [ProductController::class, 'singleProduct'])->name('product.single');
+    /**
+     * Updagte izmene product-a u bazu !!!
+     */
+    Route::post('/edit-product/save/{id}', [ProductController::class, 'save'])->name('product.save');
+});
 
 //Route::get('/', function () {
 //    return view('welcome');
